769d213fde858087da8aff129320bccc40d7d2f2
Erick <ericksilvablue@gmail.com>
2020-02-27 11:29:01 -0300

    Initial bootcamps routes && controller

--- controllers/BootcampController.js
+++ controllers/BootcampController.js
@@ -5,9 +5,30 @@ exports.index = async (req, res, next) => {
     res.json({ success: true, data: { msg: 'Get all bootcamps' } });
 }
 
-// @desc Create a bootcamp
+// @desc Create a new bootcamp
 // @route POST /api/v1/bootcamps
 // @access Public
 exports.store = async (req, res, next) => {
-    res.json({ success: true, data: { msg: 'Create a bootcamp' } });
+    res.json({ success: true, data: { msg: 'Create a new bootcamp' } });
+}
+
+// @desc Display bootcamp with specified id
+// @route GET /api/v1/bootcamps/:id
+// @access Public
+exports.show = async (req, res, next) => {
+    res.json({ success: true, data: { msg: `Display bootcamp with id ${req.params.id}` } });
+}
+
+// @desc Update bootcamp with specified id
+// @route PUT /api/v1/bootcamps/:id
+// @access Public
+exports.update = async (req, res, next) => {
+    res.json({ success: true, data: { msg: `Update bootcamp with id ${req.params.id}` } });
+}
+
+// @desc Delete bootcamp with specified id
+// @route DELETE /api/v1/bootcamps/:id
+// @access Public
+exports.$delete = async (req, res, next) => {
+    res.json({ success: true, data: { msg: `Delete bootcamp with id ${req.params.id}` } });
 }
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { index, store } = require('../controllers/BootcampController');
+const { index, store, show, update, $delete } = require('../controllers/BootcampController');
 
 router.route('/').get(index).post(store);
+router.route('/:id').get(show).put(update).delete($delete);
 
 module.exports = router;
