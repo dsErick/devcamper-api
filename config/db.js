15b661323f377ff71dd114988b41c1e1995a09a3
Erick <ericksilvablue@gmail.com>
2020-02-27 16:38:27 -0300

    MongoDB connection

--- config/db.js
+++ config/db.js
@@ -1,18 +1,14 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-           useNewUrlParser: true,
-           useFindAndModify: true,
-           useCreateIndex: false,
-           useUnifiedTopology: true
-        });
-        console.log(`Mongo connected on host ${conn.connection.host}`);
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    });
+
+    console.log(`Mongo connected: ${conn.connection.host}`);
 }
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const connectDB = require('./config/db');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect MongoDB
+connectDB();
+
 const app = express();
 
-// Connect MongoDB
-// connectDB();
-
 // Body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 // Enable cors
 // app.use(cors());
@@ -23,10 +23,17 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // Dev logging middleware
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+// if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 // Routes
 app.use('/api/v1/bootcamps', require('./routes/bootcamps'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(err.message);
+    // Close server && exit process
+    server.close(() => process.exit(1));
+})
