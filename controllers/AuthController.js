1c23c5bac2193d2da30f49c44906cc6b31871dd2
Erick <ericksilvablue@gmail.com>
2020-03-04 11:42:38 -0300

    User registration with encrypted password

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,7 +6,14 @@ const User = require('../models/User');
 // @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
+    const { name, email, password, role } = req.body;
+    
+    // Create user
+    const user = await User.create({
+        name, email, password, role
+    });
+    
     res.status(200).json({
-        success: true
+        success: true,
     });
 });
