387846fb4c1557d8c5c3cdf54cb4be504a886347
Erick <ericksilvablue@gmail.com>
2020-03-09 14:35:45 -0300

    Get single review & Update seeder & Create Review for Bootcamp

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { getReviews } = require('../controllers/ReviewController');
+const { getReviews, getReview, createReview } = require('../controllers/ReviewController');
 
 const Review = require('../models/Review');
 const advancedResults = require('../middleware/advancedResults');
@@ -8,6 +8,9 @@ const { protect, authorize } = require('../middleware/auth');
 
 
 router.route('/')
-    .get(advancedResults(Review, { path: 'bootcamp', select: 'name description' }), getReviews);
+    .get(advancedResults(Review, { path: 'bootcamp', select: 'name description' }), getReviews)
+    .post(protect, authorize('user', 'admin'), createReview);
+router.route('/:id')
+    .get(getReview)
 
 module.exports = router;
